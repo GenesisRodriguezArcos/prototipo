c060cdb9b7af08ca72a5b135088b1c12ca8afd29
Genesis Rodriguez <genesis.rodriguez.arcos@vallegrande.edu.pe>
2025-08-19 21:40:52 +0000

    segunda clase

--- backend/run.py
+++ backend/run.py
@@ -1,3 +1,26 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import flask_sqlalchemy
 from flask_cors import CORS
+from config import SQALCHEMY_DATABASE_URI
+app = Flask(__name__)
+#configuración con cors
+CORS(app, recurse=(r"/*":{"origins":"*"}))
+
+app.config['SQLALCHEMY_DATABASE_URI']=SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
+db = SQLAlchemy(app) 
+
+#Definir la base de datos 
+class Productos(db.Model):
+    __tablename__= 'productos'
+    id = db.column(db.Integer,primary_key=tru)
+    nombre = db.column(db.String.(100),nullable=False)
+    precio = db.column(db.Float..nullable=False)
+    def to_dict(self)
+    return{
+        'id': self.id,
+        'now,bre': self.nombre,
+        'precio': self.precio,
+    }
+    #Crear las
+    With 
